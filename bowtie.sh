c72577c8feae54a4c90bdf015ab0c5ed4e6e0204
Lisa Chamberland <chamberlandlisa8@gmail.com>
2022-05-23 19:55:14 -0700

    Update bowtie.sh

--- bowtie.sh
+++ bowtie.sh
@@ -19,6 +19,40 @@
 #SBATCH --mail-type=ALL                          
 #SBATCH --mail-user=lchamberland@ucdavis.edu     
 
+###################################################
 module load bowtie2
 
 source activate bowtie2
+###################################################
+### Index the reference genome
+#Index our reference file into a file that bowtie will understand. The last "bowtie" in the command below is simply a prefix in your name. Make sure you genome is in the directory when you execute the commmand.
+
+bowtie2-build your_reference_genome.fasta bowtie2
+###################################################
+#map to reference- Here you only need to specify the prefix (-x), not each individual files. -1 are your R1 reads, -2 are your R2 reads -S is the output samfile 
+
+bowtie2 --very-fast-local -x /folder/with/index/files/bowtie2 -1 /clean/read1/files_R1.fastq -2 /clean/read1/files_R2.fastq -S /directory/to/samfile/samfile.sam
+
+#to loop the command across all reads use 
+#for sample in `ls /media/sample/fastqfiles/*R1.fastq.gz`
+#do
+#dir="/media/sample/fastqfiles"
+#base=$(basename $sample "_R1.fastq")
+#bowtie2 -x path_to_my_index -1 ${dir}/${base}_R1.fastq.gz -2 ${dir}/${base}_R2.fastq.gz -S ${dir}/${base}.sam
+#done
+
+INPUT_DIRECTORY=/mnt/home/vascokar/mastitis_study/trimmed
+
+module load SAMtools
+
+source activate samtools
+
+cd $INPUT_DIRECTORY
+
+for f in *_R1_paired.fastq.gz
+do
+n=${f%%_R1_paired.fastq.gz} 
+bowtie2 –-threads 5 -x $INPUT_DIRECTORY/ARS-UCD1.2 -1 $INPUT_DIRECTORY/${n}_R1_paired.fastq.gz -2 $INPUT_DIRECTORY/${n}_R2_paired.fastq.gz -S $INPUT_DIRECTORY/${n}_R1_host_mapped_and_unmapped.sam
+done
+ 
+
